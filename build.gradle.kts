ca7f4d19b1cbc1e3b26eb2af13aeb8a057db0867
daejoon <kkode1911@gmail.com>
2024-09-08 15:04:30 +0900

    Update Gradle and Spring Boot versions
    - Update Spring Boot to 3.3.3
    - Update dependency-management to 1.1.6
    - Update gradle to 8.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "com.ddoong2"
@@ -20,7 +20,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-    testCompileOnly ("org.projectlombok:lombok")
+    testCompileOnly("org.projectlombok:lombok")
     testAnnotationProcessor("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
